94987c1f8eab91f1c038aab4e92cab7e6628de5b
frakser123 <92803997+frakser123@users.noreply.github.com>
2021-11-22 20:47:32 +0100

    Update glowna.php

--- glowna.php
+++ glowna.php
@@ -6,45 +6,71 @@
     <title>sklep z asortymentem narciarskim</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/glowna.css" rel="stylesheet">
+    <link href="glowna.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-    <div id="menu">
-    <p> menu ale nie moglem nic fajnego znalesc na bootsrtapie wiec trzeba cos wykombinowac</p>
-    </div>
-    <div id="slider">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="piersze zdjecie">
+    <div class="container">
+        <div id="slider">
+        <div class="row">
+            <div class="col-12">
+                <div id="slider">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="zdjecia\narciarz.png" class="d-block w-100" alt="pierwsze zdjecie">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="zdjecia\narty.jpg" class="d-block w-100" alt="drugie zdjecie">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="zdjecia\lyzwy.jpg" class="d-block w-100" alt="trzecie zdjecie">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="zdjecie drugie">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="zdjecie trzecie">
-                </div>
+</div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
     </div>
-
-            <div>
-                <p>zawartosc</p>
+    <div class="row">
+        <div class="col-3">
+            <div id="kategorie">
+                <p>narty</p><br>
+                <p>łyżwy</p><br>
+                <p>kombinezony</p><br>
+                <p>kaski</p><br>
+                <p>gogle</p><br>
+                <p></p><br>
             </div>
-
+        </div>
+        <div class="col-9">
+            <div id="zawartosc">
+                <p>jest dobrze</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
             <div id="stopka">
                 <P>stopka</p>
             </div>
+        </div>
+    </div>
+    </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
