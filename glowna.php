835914aa846387b6c85dc223498502e61431e70d
Koko20026 <92803129+Koko20026@users.noreply.github.com>
2021-11-29 20:25:23 +0100

    Update glowna.php

--- glowna.php
+++ glowna.php
@@ -5,36 +5,36 @@
     <title>sklep z asortymentem narciarskim</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="glowna.css" rel="stylesheet">
-    <link href="polacz.php">
+    <link href="polacz.php" >
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="glowna.css" rel="stylesheet" >
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #3CB371;">
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown" >
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="glowna.php"><img src="logo" alt="Logo" ></a>
+                        <a class="nav-link active" aria-current="page" href="glowna.php"><img src="img\logo.png" alt="Logo" ></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="produkty.php">Strona produktu</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="koszyk.php">koszyk</a>
+                        <a class="nav-link active" aria-current="page" href="koszyk.php">Koszyk</a>
                     </li>
 
                 </ul>
                 <div class="log">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="logowanie.php">logowanie</a>
+                            <a class="nav-link active" aria-current="page" href="logowanie.php">Logowanie</a>
                         </li>
                     </ul>
                 </div>
@@ -44,17 +44,17 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-xl-12 col-lg-12 col-md-12">
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="col-xl-12 col-lg-12 col-md-12" >
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="height: 500px;">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="..." class="d-block w-100" alt="...">
+                            <img src="img\reklama1.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="..." class="d-block w-100" alt="...">
+                            <img src="img\reklama2.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="..." class="d-block w-100" alt="...">
+                            <img src="img\reklama3.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
