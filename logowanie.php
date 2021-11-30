3e55825a0465f0ff830e008c187f511110feb9c9
frakser123 <92803997+frakser123@users.noreply.github.com>
2021-11-30 19:50:20 +0100

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -2,41 +2,47 @@
 <html lang="pl">
 
 <head>
-    <title></title>
+    <title>logowanie</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="logowanie.css" rel="stylesheet">
     <link href="polacz.php">
-
 </head>
 
 <body>
-    <div id="zawartosc">
+<?php
+include "polacz.php";
+if(isset($_POST['login']))
+{
+    $login = $conn->query("SELECT * FROM dane_logowania WHERE email ='" . $_POST['email'] . "' LIMIT 1");
+    $user = $login->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        if (password_verify($_POST['haselko'], $user['haslo'])){
+ die("<h3>Uzytkownik zalogowany pomyslnie</h3>");
+ }else{
+ echo "<h3>Nieprawidlowe haslo</h3>";
+ }
+ }else{
+ echo "<h3>Nie znaleziono uzytkownika</h3>";
+ }
+
+}
+?>
+ <div id="zawartosc">
+        <h1>Logowanie</h1>
         <div id="formularz">
-            <form>
-                <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div id="przycisk" class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary">zaloguj sie</button>
-        </div>
-        <div id="rejestrowanie">
-            <p>Jeżeli nie posiadasz konta <a href="rejestracja.php">zarejestruj sie </a></p>
-        </div>
-    </div>
+        <h1>Login</h1>
+<form method="post">
+ <input type="text" name="email" placeholder="Email">
+ <input type="password" name="haselko" placeholder="Password">
+ <button type="submit" name="login">Zaloguj</button>
+</form>
+            <div id="rejestrowanie">
+                <p>Jeżeli nie posziadasz konta <a href="rejestracja.php">zarejestruj sie </a></p>
+            </div>
+        </div> 
 </body>
 
 </html>
