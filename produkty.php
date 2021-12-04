88fe43cc32bd72b079c30aa55dd30cc79b2a7105
frakser123 <92803997+frakser123@users.noreply.github.com>
2021-12-04 18:19:04 +0100

    Update produkty.php

--- produkty.php
+++ produkty.php
@@ -3,15 +3,21 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link href="polacz.php">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-
   <title>produkty</title>
+  <link href="produkty.css" rel="stelysheet">
+ 
 </head>
-
 <body>
+  <?php
+  @include "polacz.php";
+$kwerenda = $conn->query("SELECT * FROM kategorie");
+  ?>
+
+
+
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"></a>
@@ -44,6 +50,8 @@
       </div>
     </div>
   </nav>
+
+
   <div class="container">
     <div class="row">
       <div class="col-xl-12 col-lg-12 col-md-12">
@@ -69,22 +77,22 @@
           </button>
         </div>
       </div>
-      <div class="zawartosc" class="col-xl-8 col-lg-12 col-md-12">
+      <div class="kategoria col-xl-4 col-lg-12 col-md-12">
         <p>kategorie</p>
-        <ul class="list-group">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
-          <li class="list-group-item">A fourth item</li>
-          <li class="list-group-item">And a fifth one</li>
-        </ul>
+        <?php
+          echo "<ul class='kat'>kategorie";
+          foreach($kwerenda as $row){
+            echo "<li>$row[kategoria]</li>";
+          }
+          echo "</ul>";
+        ?>
       </div>
-      <div class="zawartosc" class="col-xl-8 col-lg-12 col-md-12">
+      <div class="produkty col-xl-8 col-lg-12 col-md-12">
         <div class="produkt">
           <div id="zdjecie">
             zdjecie
           </div>
-          <div id="cena">
+          <div id="cena"><ul>
             cena
           </div>
           <div id="koszyk">
@@ -126,9 +134,7 @@
         </div>
       </div>
     </div>
-
   </div>
-
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
@@ -138,21 +144,17 @@
       </a>
       <span class="text-muted">© 2021 Company, Inc</span>
     </div>
-
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
       <li class="ms-3"><a class="text-muted" href="#">
           <p>regulamin<p>
         </a></li>
       <li class="ms-3"><a class="text-muted" href="#">
-          <p>polityka<prywatności< /p>
+          <p>polityka<prywatności</p>
         </a></li>
-
     </ul>
   </footer>
   </div>
   </div>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
-
 </html>
