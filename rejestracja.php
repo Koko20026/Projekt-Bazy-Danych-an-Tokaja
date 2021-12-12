17734fbde4dcf5c8cef65a8ed5408ff46e1bcacf
frakser123 <92803997+frakser123@users.noreply.github.com>
2021-12-12 22:16:12 +0100

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -5,9 +5,10 @@
     <title>rejestracja</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="rejestracja.css" rel="stylesheet">
+    <link href="logowanie.css" rel="stylesheet">
     <link href="polacz.php">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -20,7 +21,7 @@
         $sth = $conn->prepare('INSERT INTO dane_logowania (haslo,email) VALUE(:haslo, :email)');
         $sth->bindValue(':email', $email, PDO::PARAM_STR);
         $sth->bindValue(':haslo', $hashPassword, PDO::PARAM_STR);
-        $sth->execute();
+        $sth ->execute();
         header('Location: http://localhost/projekt/logowanie.php');
     }
     ?>
